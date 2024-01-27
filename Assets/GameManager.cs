fb21476f06526a93c7853d1942cee3b8c8264ee4
Alan Ly <alanly01@utexas.edu>
2024-01-26 18:07:55 -0600

    Menu implementation

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,11 +16,37 @@ public class GameManager : MonoBehaviour
             if (pauseMenu.activeSelf)
             {
                 pauseMenu.SetActive(false);
+                Time.timeScale = 1.0f;
+                UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+                UnityEngine.Cursor.visible = false;
             }
             else
             {
                 pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+                Cursor.lockState = CursorLockMode.None;
+                UnityEngine.Cursor.visible = true;
             }
+        }
+    }
+
+    public void ButtonsPressed(Button button)
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        switch (button.name)
+        {
+            case "Resume":
+                UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+                UnityEngine.Cursor.visible = false;
+                break;
+            case "Restart":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                break;
+            case "Quit":
+                SceneManager.LoadScene(0);
+                break;
+
         }
     }
 }
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject creditScreen;
+
+    public void ButtonsPressed(Button button)
+    {
+        switch (button.name)
+        {
+            case "Play":
+                SceneManager.LoadScene(1);
+                break;
+            case "Credits":
+                creditScreen.SetActive(true);
+                break;
+            case "Back":
+                // Make everything false here
+                creditScreen.SetActive(false);
+                break;
+        }
+    }
+}
